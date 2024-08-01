b2f66936137643d15b5277e0e394394ea469a21b
Juan Carlos Villero <juancarlosvo@gmail.com>
2024-07-31 23:54:53 -0500

    Configurando pruebas de integracion

---
+++ Emptelco.Api.IntegrationTest/Base/CustomWebApplicationFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emptelco.Api.IntegrationTest.Base
+{
+    public class CustomWebApplicationFactory<TStartup>
+        : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            
+        }
+
+        public HttpClient GetAnonymousClient()
+        {
+            return CreateClient();
+        }
+    }
+}
